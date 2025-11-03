539bfb526a2ceb33253e24669b83e4205a3d1814
hewenyu <he875027547@126.com>
2025-11-04 01:05:15 +0800

    完成登录页面绘制

--- scrm-front/src/view/LoginView.vue
+++ scrm-front/src/view/LoginView.vue
@@ -6,12 +6,34 @@
     -->
     <el-aside width="200px">
       <img src="../assets/loginBox.svg">
-      <p class="loginTitle">欢迎使用SCRM系统</p>
+      <p class="loginLogoText">欢迎使用SCRM系统</p>
     </el-aside>
     <!--
       右侧
     -->
-    <el-main>Main</el-main>
+    <el-main>
+
+      <div class="loginTitle">欢迎登陆</div>
+
+      <el-form :model="form" label-width="120px">
+        <el-form-item label="账号">
+          <el-input v-model="form.name"/>
+        </el-form-item>
+
+        <el-form-item label="密码">
+          <el-input v-model="form.name"/>
+        </el-form-item>
+
+        <el-form-item>
+          <el-checkbox label="记住密码" name="rememberPwd"/>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="primary">登  录</el-button>
+        </el-form-item>
+
+      </el-form>
+    </el-main>
   </el-container>
 
 </template>
@@ -22,7 +44,18 @@ import {defineComponent} from "vue"
 // 导出当前组件
 export default defineComponent({
   // 组件名称
-  name: "LoginView"
+  name: "LoginView",
+
+  // 定义页面使用到的变量
+  data() {
+    return {
+      // 对象变量的定义是一个 {}
+      form: {
+        // 字符串变量的定义是 ''
+        name: ''
+      }
+    }
+  }
 })
 </script>
 
@@ -44,9 +77,26 @@ export default defineComponent({
   height: 400px;
 }
 
-.el-aside .loginTitle {
+.el-aside .loginLogoText {
   color: white;
   font-size: 30px;
+}
+
+.el-form {
+  width: 60%;
+  margin: auto;
+}
+
+.el-main .loginTitle {
+  text-align: center;
+  margin-top: 100px;
+  margin-bottom: 40px;
+  font-weight: bold;
+  font-size: 25px;
+}
+
+.el-button {
+  width: 100%;
 }
 
 </style>
