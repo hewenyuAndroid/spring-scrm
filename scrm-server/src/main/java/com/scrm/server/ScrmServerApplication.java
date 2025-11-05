6940862e80de0d55870fb3808fce9689c81ee62a
hewenyu <he875027547@126.com>
2025-11-05 20:02:09 +0800

    创建scrm-server module

---
+++ scrm-server/src/main/java/com/scrm/server/ScrmServerApplication.java
@@ -0,0 +1,13 @@
+package com.scrm.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrmServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScrmServerApplication.class, args);
+    }
+
+}
---
+++ scrm-server/src/test/java/com/scrm/server/ScrmServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scrm.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrmServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
